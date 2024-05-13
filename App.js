a606419a83a31ac061e5d460884bcfd26a1720b0
Aparecida-Lima19 <ci.lima.silva22@gmail.com>
2024-05-12 23:35:59 -0300

    Subindo novas alterações

--- App.js
+++ App.js
@@ -5,17 +5,22 @@ import Title from './scr/components/title';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Title/>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+      colors={['#00FFFF', '#1782AF']} 
+      style={styles.container}
+    >
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Title />
+        <StatusBar style="auto" />
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00FFFF',
+    
     alignItems: 'center',
     justifyContent: 'center',
   },
