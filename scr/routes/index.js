854ae75b5b4da5787cfccfeb326edfad813e80de
Aparecida-Lima19 <ci.lima.silva22@gmail.com>
2024-05-15 11:10:10 -0300

    Deu erro...
    Criando V.03

--- scr/routes/index.js
+++ scr/routes/index.js
@@ -1,8 +1,8 @@
 import { createNativeStackNavigator} from '@react-navigation/native-stack'
 
-import Title from '../components/title';
-import Welcome from'../components/welcome'
-import Usuario from '../components/usuarios';
+
+
+
 
 const Stack = createNativeStackNavigator();
 
