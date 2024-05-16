e383dec7352ab67bd23ddd216b87bdc9c2363cb1
Aparecida-Lima19 <ci.lima.silva22@gmail.com>
2024-05-16 16:08:10 -0300

    Não vai

--- scr/components/usuario/index.js
+++ scr/components/usuario/index.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { View, Text } from "react-native"
+import { useFonts, FasterOne_400Regular } from '@expo-google-fonts/faster-one';
+import AppLoading from 'expo-app-loading';
 
 export default function Usuario(){
-    return(
-        <View>
-            <Text>Tela Usuario</Text>
-            
-        </View>
-    );
+    let [fontsLoaded] = useFonts({
+        FasterOne_400Regular,
+      });
+    
+      if (!fontsLoaded) {
+        return <AppLoading />;
+      }
+
+    return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Tela Usuario</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      fontFamily: 'FasterOne_400Regular',
+      fontSize: 45,
+    },
+  });
